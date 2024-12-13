bf4ed84028fb27246871ad726f0a744837832788
Nissan130 <mdnissanali130@gmail.com>
2024-12-13 08:57:11 +0600

    Changed

--- PHP-class6/lab-task6-email-verification/config.php
+++ PHP-class6/lab-task6-email-verification/config.php
@@ -3,7 +3,7 @@
 return [
     'host' => 'smtp.gmail.com', // Your SMTP server
     'username' => 'nishoahmed570@gmail.com', // Your email address
-    'password' => 'ppbjzytgfzlxdase', // Your email password
+    'password' => '', // Your email password
     'port' => 587, // Typically 587 for TLS or 465 for SSL
     'from_email' => 'nishoahmed570@gmail.com', // Sender email address
     'from_name' => 'Nisho Ahmed', // Sender name
